0be44ac05dd1ddcc7e45496d7c8f0354434b535d
Gustavo Lima <163158193+gustavo14lima@users.noreply.github.com>
2025-09-12 11:56:05 -0300

    add = exercicios-praticos_2

---
+++ repo-uc20/3° Trimestre/AC1 - Aulas 0 à 6/Exercícios Práticos/Exercícios Práticos em Sala/aula01.py
@@ -0,0 +1,5 @@
+'''
+    Altere o título da janela para "UC20 - Aula 01".
+    Mude a lista mensagens para conter 3 nomes de colegas. Mostre o segundo nome no QLabel.
+    Crie uma função que receba um dicionário {"nome": "André", "turma": "3ºC"} e mostre os valores no QLabel.
+'''
